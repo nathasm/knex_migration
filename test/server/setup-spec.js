1224a6b257088b61dbc49c9178f630163699fc4a
Nathan Smith <nathasm@gmail.com>
2015-01-21 08:21:59 -0500

    Update setup-spec.js

--- test/server/setup-spec.js
+++ test/server/setup-spec.js
@@ -2,7 +2,7 @@ var bookshelf = require('../../server/config/bookshelf');
 
 before(function (done) {
   return bookshelf.knex.schema.dropTableIfExists('companies').dropTableIfExists('users').then(function() {
-    bookshelf.knex.migrate.latest().then(function() {
+    return bookshelf.knex.migrate.latest().then(function() {
       return bookshelf.knex.seed.run().then(function() {
         done();
       });
